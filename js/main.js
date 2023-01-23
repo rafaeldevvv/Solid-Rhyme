83c70ea0a4370820914c81021800126a45362c57
rafaeldevvv <115797599+rafaeldevvv@users.noreply.github.com>
2023-01-22 21:00:22 -0300

    Shop

--- js/main.js
+++ js/main.js
@@ -1,9 +1,19 @@
-document.getElementsByClassName("toggleMenu")[0].addEventListener("click", () => {
-   let menu = document.getElementById('nav');
+document
+  .getElementsByClassName("toggleMenu")[0]
+  .addEventListener("click", () => {
+    let menu = document.getElementById("nav");
 
-   if (nav.className == "hide") {
+    if (nav.className == "hide") {
       nav.className = "show";
-   } else {
+    } else {
       nav.className = "hide";
-   }
-});
+    }
+  });
+
+let imgs = document.querySelectorAll(".product img");
+for (let img of imgs) {
+  img.onload = function () {
+    let width = img.width;
+    img.style.height = width + "px";
+  };
+}
